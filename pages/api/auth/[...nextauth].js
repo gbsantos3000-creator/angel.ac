0b343fd32743bdb23e139df3cf821fa7f6c50d49
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-06 22:03:16 -0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,31 +5,20 @@ export const authOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_SECRET,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
 
   callbacks: {
     async session({ session, token }) {
+
       session.user.id = token.sub
 
-      // 🔥 AQUI VOCÊ DEFINE O PLANO (simples por enquanto)
-      // depois pode vir de banco
+      // PLANOS
 
-      const plan = "mensal" // muda aqui depois
+      session.user.plan = "Lifetime"
 
-      let days = 30
-
-      if (plan === "trimestral") days = 90
-      if (plan === "anual") days = 365
-      if (plan === "lifetime") days = null
-
-      const now = Date.now()
-
-      session.user.plan = plan
-      session.user.expiresAt = days
-        ? now + days * 24 * 60 * 60 * 1000
-        : null
+      session.user.expire = "Never expires"
 
       return session
     },
