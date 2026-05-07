5a8a27e5e1cf74d9e6cef01184205f9c6f9e0fb6
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-07 02:46:23 -0300

    Update discord.js

--- pages/api/auth/discord.js
+++ pages/api/auth/discord.js
@@ -1,1 +1,12 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    response_type: "code",
+    scope: "identify email",
+  });
 
+  res.redirect(
+    `https://discord.com/oauth2/authorize?${params.toString()}`
+  );
+}
