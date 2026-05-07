6c73039610b20d00324bc5edd2c91bb577241257
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-07 03:10:06 -0300

    Update layout.tsx

--- pages/app/layout.tsx
+++ pages/app/layout.tsx
@@ -1,1 +1,23 @@
+import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 
+export const metadata = {
+  title: "ANGEL A.C",
+  description: "ANGEL A.C Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider>
+          {children}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
