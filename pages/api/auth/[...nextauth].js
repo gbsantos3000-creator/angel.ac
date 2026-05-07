7667b8a54bf46aa76d036e8dbc3be6f4c415e5c0
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-07 03:30:36 -0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,12 @@
-// app/api/auth/[...nextauth]/route.ts
-
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-const handler = NextAuth({
+export default NextAuth({
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-
-  pages: {
-    signIn: "/",
-  },
 });
-
-export { handler as GET, handler as POST };
