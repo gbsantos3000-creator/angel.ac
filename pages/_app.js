ccfb83cf1416869c171033049cca812bb5c573e7
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-07 03:31:00 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
