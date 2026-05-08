164d48e07d406fc31f9e806e55f2d366477e0eab
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-08 02:04:06 -0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,31 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
+const baseUrl =
+  "https://angel-ac-zocv.vercel.app";
+
 export default NextAuth({
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      clientId:
+        process.env.DISCORD_CLIENT_ID,
+
+      clientSecret:
+        process.env
+          .DISCORD_CLIENT_SECRET,
+
       authorization: {
         params: {
           scope: "identify email",
+
+          redirect_uri: `${baseUrl}/api/auth/callback/discord`,
         },
       },
     }),
   ],
 
-  secret: process.env.NEXTAUTH_SECRET,
+  secret:
+    process.env.NEXTAUTH_SECRET,
 
   session: {
     strategy: "jwt",
@@ -26,28 +37,41 @@ export default NextAuth({
   },
 
   callbacks: {
-    async jwt({ token, profile }) {
+    async jwt({
+      token,
+      profile,
+    }) {
       if (profile) {
-        token.username = profile.username;
+        token.username =
+          profile.username;
       }
 
       return token;
     },
 
-    async session({ session, token }) {
+    async session({
+      session,
+      token,
+    }) {
       if (session.user) {
-        session.user.id = token.sub;
-        session.user.name =
-          token.username || session.user.name;
+        session.user.id =
+          token.sub;
 
-        session.user.plan = "OWNER";
-        session.user.license = "LIFETIME";
+        session.user.name =
+          token.username ||
+          session.user.name;
+
+        session.user.plan =
+          "OWNER";
+
+        session.user.license =
+          "LIFETIME";
       }
 
       return session;
     },
 
-    async redirect({ baseUrl }) {
+    async redirect() {
       return `${baseUrl}/dashboard`;
     },
   },
