0e978290c5ab56952cd0e593d7753cf816a64bc8
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-08 01:18:23 -0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,19 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
+const baseUrl = "https://angel-ac-zocv.vercel.app";
+
 export default NextAuth({
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "identify email",
+          redirect_uri: `${baseUrl}/api/auth/callback/discord`,
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
