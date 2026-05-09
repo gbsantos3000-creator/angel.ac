070d50b852130e288df2d153225efc16a5d2a17c
gbsantos3000-creator <gbsantos3000@gmail.com>
2026-05-09 07:57:31 -0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,13 +6,10 @@ export default NextAuth({
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: "identify email",
-        },
-      },
     }),
   ],
+
+  secret: process.env.NEXTAUTH_SECRET,
 
   session: {
     strategy: "jwt",
@@ -23,5 +20,16 @@ export default NextAuth({
     maxAge: 30 * 24 * 60 * 60,
   },
 
-  secret: process.env.NEXTAUTH_SECRET,
+  cookies: {
+    sessionToken: {
+      name: "__Secure-next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+        maxAge: 30 * 24 * 60 * 60,
+      },
+    },
+  },
 })
